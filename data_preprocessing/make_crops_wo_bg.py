f2514c4d6128c546fd36f09137ea6b35f5840401
terry-1031 <cchh103131@gmail.com>
2025-11-03 17:33:07 +0900

    크롭 후 배경 제거

--- data_preprocessing/make_crops_wo_bg.py
+++ data_preprocessing/make_crops_wo_bg.py
@@ -2,7 +2,9 @@ from rembg import remove
 from PIL import Image
 from tqdm import tqdm
 import numpy as np
+from pathlib import Path
 
+# make_crops.py를 써 crop된 이미지의 배경 제거
 def u2net_remove_folder(in_dir: Path, out_dir: Path, suffix="_nobg", # in_dir : crops 폴더, out_dir : 저장 폴더, suffix : 접미사
                         alpha_matting=True,
                         alpha_matting_foreground_threshold=240,
